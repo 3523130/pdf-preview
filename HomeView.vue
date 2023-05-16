6c6f55cf9536219a569038d4d975e7dbe93c7d0e
luofuhua <3523130@qq.com>
2023-05-16 17:57:07 +0800

    finish

---
+++ HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="">
+      <PDFView :pdfUrl="jsPdf"></PDFView>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import PDFView from './PdfView.vue'
+
+const jsPdf = ref('CheckRelation.pdf');
+</script>
+
+<style scoped>
+
+</style>
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pdf-preview
+
+npm i vue-pdf-embed -S
+
+npm i vue3-pdfjs -s
